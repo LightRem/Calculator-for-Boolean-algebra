21b19bc9a7283b3d321df388a2aaec6cfd7f2a03
Rem <romanylko@gmail.com>
2026-06-02 18:25:29 +0300

    Initialize main application with Qt framework

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <windows.h>
+#include <clocale>
+#include <QApplication>
+#include "mainwindow.h"
+using namespace std;
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
